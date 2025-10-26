01de49eef195ffca2d1400db2958b3627420490c
feirahisham <feirahisham@gmail.com>
2025-10-26 11:58:17 +0800

    changing to project folder

--- 1_TwoSum.cs
+++ 1_TwoSum.cs
@@ -1,18 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-// This is the class structure used by LeetCode
 public class Solution
 {
-    /**
-     * LeetCode Problem #1: Two Sum
-     * Given an array of integers `nums` and an integer `target`, return indices
-     * of the two numbers such that they add up to `target`.
-     * * @param nums The array of numbers.
-     * @param target The target sum.
-     * @return An array of two indices [index1, index2].
-     */
     public static int[] TwoSum(int[] nums, int target)
     {
         int x = 0, y = 0;
@@ -29,51 +16,5 @@ public class Solution
         }
 
         return [x, y];
-    }
-}
-
-// This is the runner class needed to actually execute and test the solution
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        // --- Setting up the test environment ---
-
-        // 1. Instantiate the Solution class
-        Solution solution = new Solution();
-
-        // 2. Define the inputs (This is where the missing arguments usually occur!)
-        int[][] nums = [
-            [2, 7, 11, 15]
-            , [3, 2, 4]
-            , [3, 3]
-        ];
-
-        int[] target = [9, 6, 6];
-        
-        Console.WriteLine("--- Two Sum Problem Solver ---");
-        Console.WriteLine($"Input Array: [{string.Join(", ", nums)}]");
-        Console.WriteLine($"Target Value: {target}");
-
-        try
-        {
-            for (int i = 0; i < nums.Length; i++)
-            {
-                // 3. Call the TwoSum method with the defined arguments
-            int[] result = Solution.TwoSum(nums[i], target[i]);
-
-            // 4. Display the results
-            if (result.Length == 2)
-            {
-                Console.WriteLine("\nSolution Found:");
-                Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
-                Console.WriteLine($"Check: {nums[i][result[0]]} + {nums[i][result[1]]} = {nums[i][result[0]] + nums[i][result[1]]}");
-            }
-            }
-        }
-        catch (ArgumentException ex)
-        {
-            Console.WriteLine($"\nError: {ex.Message}");
-        }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int[][] nums = [
+    [2, 7, 11, 15]
+    , [3, 2, 4]
+    , [3, 3]
+];
+
+int[] target = [9, 6, 6];
+
+Console.WriteLine("--- Two Sum Problem Solver ---");
+
+try
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.WriteLine($"===========================");
+        Console.WriteLine($"Input Array: [{string.Join(", ", nums[i])}]");
+        Console.WriteLine($"Target Value: {target[i]}");
+        
+        int[] result = Solution.TwoSum(nums[i], target[i]);
+
+
+        if (result.Length == 2)
+        {
+            Console.WriteLine("\nSolution Found:");
+            Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+            Console.WriteLine($"Check: {nums[i][result[0]]} + {nums[i][result[1]]} = {nums[i][result[0]] + nums[i][result[1]]}");
+        }
+        Console.WriteLine($"===========================");
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"\nError: {ex.Message}");
+}
